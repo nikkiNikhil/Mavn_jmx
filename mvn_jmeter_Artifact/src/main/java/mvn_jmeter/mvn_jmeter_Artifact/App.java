21f623e7f1c8830799f33e667fe10d3d29f83e56
nikkiNikhil <nikhil.nikki09@gmail.com>
2019-03-21 15:56:10 +0530

    MVN JMX PROJECT

---
+++ mvn_jmeter_Artifact/src/main/java/mvn_jmeter/mvn_jmeter_Artifact/App.java
@@ -0,0 +1,13 @@
+package mvn_jmeter.mvn_jmeter_Artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
